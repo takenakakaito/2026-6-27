e074fa8b4a07be3d7e6e8cb19e974fc4b2438311
takenakakaito <ktc25a32a0010@edu.kyoto-tech.ac.jp>
2026-06-27 15:50:51 +0900

    途中であきらめ

--- ConsoleApplication1/ConsoleApplication1/judge.cpp
+++ ConsoleApplication1/ConsoleApplication1/judge.cpp
@@ -29,18 +29,34 @@ bool judge(int answer1, int answer2, int answer3, int answer4, int input1, int i
     if (input1==answer1||input1==answer2||input1==answer3)
     {
 		blow++;
+        if (answer4== input1)
+        {
+            blow--;
+        }
     }
     if (input2 == answer1 || input2 == answer2 || input2 == answer4)
 	{
         blow++;
+        if (answer3 == input2)
+        {
+            blow--;
+        }
     }
     if (input3 == answer1 || input3 == answer4 || input3 == answer3)
     {
         blow++;
+        if (answer2 == input3)
+        {
+            blow--;
+        }
     }
     if (input4 == answer4 || input4 == answer2 || input4 == answer3)
     {
         blow++;
+        if (answer1 == input4)
+        {
+            blow--;
+        }
 	}
 	printf("blow:%d\n", blow);
 	printf("hit:%d\n", hit);
